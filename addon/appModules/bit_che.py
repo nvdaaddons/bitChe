ddbc414084e102fe58eb931c934f6c7c532ce65c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2013-07-17 16:04:46 +0200

    Experimental branch to test: added script contextMenu to a customized class for items.

--- addon/appModules/bit_che.py
+++ addon/appModules/bit_che.py
@@ -1,10 +1,17 @@
+# -*- coding: UTF-8 -*-
+
 import appModuleHandler
 import controlTypes
 import api
 #import ui
 import winUser
+from NVDAObjects.IAccessible.sysListView32 import ListItem
 
 class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.role == controlTypes.ROLE_LISTITEM:
+			clsList.insert(0, ResultsItem)
 
 	def event_gainFocus(self, obj, nextHandler):
 		if obj.windowClassName == 'ThunderRT6UserControl' and len(obj.children) > 0 and obj.children[0].role == controlTypes.ROLE_LIST:
@@ -13,10 +20,11 @@ class AppModule(appModuleHandler.AppModule):
 			obj.children[0].setFocus()
 		nextHandler()
 
+class ResultsItem(ListItem):
+
 	def script_contextMenu(self, gesture):
-		obj=api.getFocusObject()
-		api.moveMouseToNVDAObject(obj)
-		api.setMouseObject(obj)
+		api.moveMouseToNVDAObject(self)
+		api.setMouseObject(self)
 		winUser.mouse_event(winUser.MOUSEEVENTF_RIGHTDOWN,0,0,None,None)
 		winUser.mouse_event(winUser.MOUSEEVENTF_RIGHTUP,0,0,None,None)
 
