29a37fa57e67f90c1749989477722f4803682b9f
Noelia Ruiz Martínez <nrm1977@gmail.com>
2013-07-18 09:30:33 +0200

    Added customized classes: ResultsList and ResultsListItem.

--- addon/appModules/bit_che.py
+++ addon/appModules/bit_che.py
@@ -1,27 +1,41 @@
 # -*- coding: UTF-8 -*-
-
 import appModuleHandler
 import controlTypes
 import api
-#import ui
 import winUser
+from NVDAObjects.IAccessible.sysListView32 import List, ListItem
 
-class AppModule(appModuleHandler.AppModule):
+class ResultsList(List):
 
-	def event_gainFocus(self, obj, nextHandler):
-		if obj.windowClassName == 'ThunderRT6UserControl' and len(obj.children) > 0 and obj.children[0].role == controlTypes.ROLE_LIST:
-			obj.name = obj.children[0].name
-			obj.role = obj.children[0].role
-			obj.children[0].setFocus()
-		nextHandler()
+	def event_gainFocus(self):
+		# the real list of results
+		childList=self.children[0]
+		# adjust the object properties
+		self.name=childList.name
+		self.role=childList.role
+		self.children=childList
+		# move the focus on the real list
+		childList.setFocus()
+
+class ResultListItem(ListItem):
+
+	def event_gainFocus(self):
+		super(ResultListItem, self).event_gainFocus()
 
 	def script_contextMenu(self, gesture):
-		obj=api.getFocusObject()
-		api.moveMouseToNVDAObject(obj)
-		api.setMouseObject(obj)
+		api.moveMouseToNVDAObject(self)
+		api.setMouseObject(self)
 		winUser.mouse_event(winUser.MOUSEEVENTF_RIGHTDOWN,0,0,None,None)
 		winUser.mouse_event(winUser.MOUSEEVENTF_RIGHTUP,0,0,None,None)
 
 	__gestures = {
 		"kb:applications": "contextMenu",
 	}
+
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.windowClassName == 'ThunderRT6UserControl' and len(obj.children) > 0 and obj.children[0].role == controlTypes.ROLE_LIST:
+			clsList.insert(0, ResultsList)
+		if not isinstance(obj, ListItem) and obj.role == controlTypes.ROLE_LISTITEM:
+			clsList.insert(0, ResultListItem)
